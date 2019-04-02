994f6381634942b6de7ed7eca19577e9b66e3c54
zhengkb <zkb2668977574@163.com>
2019-04-02 12:14:41 +0800

    spring-cloud，自定义eureka负载均衡策略

---
+++ cloud-demo-consume/src/main/java/cn/config/LoadBalanced.java
@@ -0,0 +1,15 @@
+package cn.config;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoadBalanced {
+    @Bean
+    public IRule ribbonRule(){
+        return new RoundRobinRule();
+    }
+
+}
--- cloud-demo-consume/src/main/java/cn/zkb/CloudDemoConsume.java
+++ cloud-demo-consume/src/main/java/cn/zkb/CloudDemoConsume.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "provider-demo",configuration = cn.config.LoadBalanced.class)
 public class CloudDemoConsume {
 
     @Bean
